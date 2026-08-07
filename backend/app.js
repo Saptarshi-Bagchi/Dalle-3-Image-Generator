3846bea2066c6d68bc38e529684d9eef31c707fa
Saptarshi Bagchi <saptarshibagchi.05@gmail.com>
2026-08-07 19:03:27 +0530

    Initial Commit

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,7 @@
+import express from "express";
+const app = express();
+const PORT = 9000;
 //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
+
+//!Start the serve
+app.listen(PORT, console.log(`Server is running on port ${PORT}`));
