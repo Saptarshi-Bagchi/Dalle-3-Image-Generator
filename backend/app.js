605d3efa1c80d23a4e0d79efc6b8ad5b823b7b73
Saptarshi Bagchi <saptarshibagchi.05@gmail.com>
2026-08-08 17:26:09 +0530

    Added Image Generation

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,31 @@
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
+import OpenAI from "openai";
+
 const app = express();
 const PORT = 9000;
-//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
 
+//!Configure openai
+const openai = new OpenAI({apiKey: process.env.OPENAI_KEY});
+//Middleware
+app.use(express.json());
+
+//!Route
+app.post('/generate-image', async(req, res) => {
+    const {prompt} = req.body;
+    try{
+        const imageResponse = await openai.images.generate({
+            model:"gpt-image-1",
+            prompt,
+            n:1,
+            size:'1024x1024'
+        })
+        res.json(imageResponse.data[0].url);
+    } catch (error) {
+        console.error(error);
+        res.json({ message: "Error generating image"});
+    }
+})
 //!Start the serve
 app.listen(PORT, console.log(`Server is running on port ${PORT}`));
